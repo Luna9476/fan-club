ce5a3a86fde0b881f3359cfa719c78efa8fdee3e
Luna <luleinju9476@gmail.com>
2022-03-23 15:12:27 -0700

    1. revert metamask change 2. star publish form refactor

--- src/components/Manage.js
+++ src/components/Manage.js
@@ -1,60 +1,76 @@
 import React, { Component } from 'react'
 import InputGroup from 'react-bootstrap/InputGroup'
 import Form from 'react-bootstrap/Form'
-import 'bootstrap/dist/css/bootstrap.css';
-import {Button, FormControl } from "react-bootstrap";
+import { Button, Container} from "react-bootstrap";
 
 
-export default class Manage extends Component {
-    render() {
-        return (
-            <div style={{
-                backgroundColor: "#ffde00",
-                display: 'block',
-                width: 700,
-                // color: #00F,
-                padding: 30
-            }}>
-                <h4>Post Your Idol's Information</h4>
-                <InputGroup size="sm" className="mb-3">
-                    <InputGroup.Text id="basic-addon1">Id</InputGroup.Text>
-                    <FormControl
-                        placeholder="Idol's Id"
-                        aria-label="Idol's Id"
-                        aria-describedby="Idol's Id"
-                    />
-                    
-                </InputGroup>
+export default function Manage() {
+    return (
+        <Container className='form-border'>
+            <h3>Post Idols</h3>
+            <Form >
+                <Form.Group className="mb-3 input-group input-group-outline" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email"/>
+                </Form.Group>
 
-                <InputGroup size="sm" className="mb-3">
-                    <InputGroup.Text id="basic-addon2">Name</InputGroup.Text>
-                    <FormControl
-                        placeholder="Idol's Name"
-                        aria-label="Idol's Name"
-                        aria-describedby="Idol's Name"
-                    />
-                </InputGroup>
-
-                <InputGroup className="mb-3">
-                    <InputGroup.Text id="basic-addon3">
-                        Avatar URL
-                    </InputGroup.Text>
-                    <FormControl id="basic-url" aria-describedby="basic-addon3" />
-                </InputGroup>
-
-
-                <InputGroup>
-                    <InputGroup.Text>Introdution</InputGroup.Text>
-                    <FormControl as="textarea" aria-label="With textarea" />
-                </InputGroup>
-                <br />
-
-                <Button variant="primary" type="submit">
-                    Click here to submit form
+                <Form.Group className="mb-3 input-group input-group-outline" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" />
+                </Form.Group>
+                <Form.Group className="mb-3 input-group input-group-outline" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit" onClick={() => {}}>
+                    Submit
                 </Button>
+            </Form>
+        </Container>
+
+        // <div style={{
+        //     display: 'block',
+        //     width: 700,
+        //     // color: #00F,
+        //     padding: 30
+        // }}>
+        //     <InputGroup size="sm" className="mb-3">
+        //         <InputGroup.Text id="basic-addon1">Id</InputGroup.Text>
+        //         <FormControl
+        //             placeholder="Idol's Id"
+        //             aria-label="Idol's Id"
+        //             aria-describedby="Idol's Id"
+        //         />
+
+        //     </InputGroup>
+
+        //     <InputGroup size="sm" className="mb-3">
+        //         <InputGroup.Text id="basic-addon2">Name</InputGroup.Text>
+        //         <FormControl
+        //             placeholder="Idol's Name"
+        //             aria-label="Idol's Name"
+        //             aria-describedby="Idol's Name"
+        //         />
+        //     </InputGroup>
+
+        //     <InputGroup className="mb-3">
+        //         <InputGroup.Text id="basic-addon3">
+        //             Avatar URL
+        //         </InputGroup.Text>
+        //         <FormControl id="basic-url" aria-describedby="basic-addon3" />
+        //     </InputGroup>
 
 
-            </div>
-        )
-    }
+        //     <InputGroup>
+        //         <InputGroup.Text>Introdution</InputGroup.Text>
+        //         <FormControl as="textarea" aria-label="With textarea" />
+        //     </InputGroup>
+        //     <br />
+
+        //     <Button variant="primary" type="submit">
+        //         Click here to submit form
+        //     </Button>
+
+
+        // </div>
+    )
 }
--- src/components/MetaMaskAuth.js
+++ src/components/MetaMaskAuth.js
@@ -1,133 +1,15 @@
-// import React, { useEffect, useState } from "react";
-// import { Navbar, Container, Nav, Button, Form, Modal, Row, Col } from 'react-bootstrap';
-// import { BsFillHeartFill, BsPersonCircle } from "react-icons/bs";
-// import { BiLinkExternal } from "react-icons/bi"
-// import { ethers } from 'ethers';
-
-// // import Club from '../artifacts/contracts/Club.sol/Club.json'
-
-
-
-
-// export default function MetaMaskAuth({setAdmin}) {
-//     const [userAddress, setUserAddress] = useState("");
-//     const [showModal, setShowModal] = useState(false)
-
-//     const handleClose = () => {
-//         setShowModal(false);
-//     }
-
-//     const disconnect = () => {
-//         setShowModal(false);
-//         setUserAddress("")
-//     }
-
-//     window.ethereum.on('accountsChanged', (accounts) => {
-//         setUserAddress(accounts[0])
-//     });
-
-//     const connect = async (onConnected) => {
-//         if (!window.ethereum) {
-//             alert("Get MetaMask!");
-//             return;
-//         }
-    
-//         const accounts = await window.ethereum.request({
-//             method: "eth_requestAccounts",
-//         });
-//         onConnected(accounts[0]);
-//         console.log(userAddress)
-
-//         const provider = new ethers.providers.Web3Provider(window.ethereum)
-//         const contract = new ethers.Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", Club.abi, provider)
-//         const isAdmin = await contract.isAdmin()
-//         setAdmin(isAdmin)
-        
-//     }
-
-//     return (
-//         userAddress ? (
-//             <>
-//                 <Form className="d-flex">
-//                     <Button onClick={() => setShowModal(true)}>
-//                         Connected with <Address userAddress={userAddress} />
-//                     </Button>
-//                     <Nav.Link><BsPersonCircle className='fa-2x  primary' />My Account</Nav.Link>
-//                 </Form>
-
-
-//                 <Modal show={showModal} onHide={handleClose}>
-//                     <Modal.Header closeButton>
-//                         <Modal.Title>Account</Modal.Title>
-//                     </Modal.Header>
-//                     <Modal.Body>
-//                         <Container>
-//                             <Row>
-//                                 <Col>Connected with MetaMask</Col>
-//                                 <Col className="float-end"><Button variant="outline-primary" size="sm" onClick={disconnect} className="float-end">
-//                                     Disconnect
-//                                 </Button></Col>
-//                             </Row>
-//                             <Row>
-//                                 <Col md="auto"><BsPersonCircle size={70} /></Col>
-//                                 <Col><Address userAddress={userAddress} /></Col>
-//                             </Row>
-//                             <Row>
-//                                 <Col><a href={"https://etherscan.io/address/" + userAddress}><BiLinkExternal />view</a></Col>
-
-
-//                             </Row>
-//                         </Container>
-//                         <Modal.Footer>
-//                             <Button variant="primary" onClick={handleClose}>
-//                                 Close
-//                             </Button>
-//                         </Modal.Footer>
-//                     </Modal.Body>
-//                 </Modal>
-//             </>
-
-
-//         ) : (
-
-//             <Form >
-//                 <Button onClick={() => connect(setUserAddress)}>Connect Wallet</Button>
-//             </Form>
-
-//         )
-//     );
-
-
-
-
-
-// }
-
-// function Address({ userAddress }) {
-//     return (
-//         <span>{userAddress.substring(0, 5)}…{userAddress.substring(userAddress.length - 4)}</span>
-//     );
-// }
 import React, { useEffect, useState } from "react";
 import { Navbar, Container, Nav, Button, Form, Modal, Row, Col } from 'react-bootstrap';
 import { BsFillHeartFill, BsPersonCircle } from "react-icons/bs";
 import { BiLinkExternal } from "react-icons/bi"
+import { ethers } from 'ethers';
 
-async function connect(onConnected) {
-    if (!window.ethereum) {
-        alert("Get MetaMask!");
-        return;
-    }
-
-    const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-    });
-
-    onConnected(accounts[0]);
-}
+import Club from '../artifacts/contracts/Club.sol/Club.json'
 
 
-export default function MetaMaskAuth() {
+
+
+export default function MetaMaskAuth({ setAdmin }) {
     const [userAddress, setUserAddress] = useState("");
     const [showModal, setShowModal] = useState(false)
 
@@ -144,6 +26,24 @@ export default function MetaMaskAuth() {
         setUserAddress(accounts[0])
     });
 
+    const connect = async (onConnected) => {
+        if (!window.ethereum) {
+            alert("Get MetaMask!");
+            return;
+        }
+
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+        });
+        onConnected(accounts[0]);
+        console.log(userAddress)
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const contract = new ethers.Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", Club.abi, provider)
+        const isAdmin = await contract.isAdmin()
+        setAdmin(isAdmin)
+
+    }
 
     return (
         userAddress ? (
@@ -208,3 +108,5 @@ function Address({ userAddress }) {
         <span>{userAddress.substring(0, 5)}…{userAddress.substring(userAddress.length - 4)}</span>
     );
 }
+
+
