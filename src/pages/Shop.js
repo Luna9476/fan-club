7b6950fdd4dd2c95e20dabc096d8abbde0f6066c
slinger0225 <694600580@qq.com>
2022-03-25 21:38:27 -0700

    add shop page styling

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,29 +1,61 @@
 import React, { Component } from 'react'
-import { Card, Button, Form, Col, Row } from 'react-bootstrap';
+import { Card, Form, Col, Row, Button} from 'react-bootstrap';
+
+const shopStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%'
+}
+
+const checkOutBGStyle = {
+    border:"1px solid rgb(10 10 10 / 13%)", 
+    borderRadius:"0rem", 
+    width:"100vw", 
+    height:"15vh"
+}
+const checkOutElementStyle = {
+    display:"flex",
+    flexDirection:"column",
+    justifyContent:"center",
+    marginRight:"2rem"
+}
 
 export default class Manage extends Component {
     render(){
         return(
-            <Card 
-                style={{ width: '25rem' }}>
-            <Card.Img variant="top" src="ticket.jpeg" />
-            <Card.Body>
-                <Card.Title>Fan-Vote Ticket</Card.Title>
-                <Card.Text>
-                Buy the ticket to vote your idol
-                </Card.Text>
-                <Form>
-                    <Form.Group as={Row} className="pt-3" controlId="formPlaintextEmail">
-                        <Form.Label column sm="7">
-                        <b>Quantity:</b>
-                        </Form.Label>
-                        <Col>
-                        <Form.Control type = "number" placeholder='1' style={{width:'45%'}}/>
-                        </Col>
-                    </Form.Group>
-                </Form>
-            </Card.Body>
-            </Card>
+            <div style={shopStyle}>
+                <Card
+                    style={{ width: '25vw' }}>
+                <Card.Img variant="top" src="ticket.jpeg" />
+                <Card.Body>
+                    <Card.Title>Fan-Vote Ticket</Card.Title>
+                    <Card.Text>
+                    Buy the ticket to vote your idol
+                    </Card.Text>
+                    <Form>
+                        <Form.Group as={Row} className="pt-3" >
+                            <Form.Label column sm="7" className='font-weight-bold'>
+                            Quantity:
+                            </Form.Label>
+                            <Col>
+                            <Form.Control className="border border-dark text-center" type = "number" placeholder='1' style={{width:'45%' }}/>
+                            </Col>
+                        </Form.Group>
+                    </Form>
+                </Card.Body>
+                </Card>
+                <Card style={checkOutBGStyle}>
+                    <div style = {{display:"flex", flexDirection:"row-reverse"}}>
+                        <div style = {checkOutElementStyle}>
+                            <Card.Text className = "pt-3" ><h5 style = {{marginBottom: "0rem"}}>Total Price: 3 ETH</h5></Card.Text>
+                            <Card.Text>(Available Balance: 2 ETH)</Card.Text>
+                            <Button variant="primary"> Checkout </Button>
+                        </div>
+                    </div>
+                </Card>
+            </div>
         )  
     }
 }
