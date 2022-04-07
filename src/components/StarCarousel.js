be45f44b4f77dc7c619e7a20039e96332ff49ac0
Jiangsissi720 <qianhuijiang720@163.com>
2022-04-07 00:00:37 -0700

    1. show the photos of the top three votes stars on the starcarousel  2. modify the myaccount page of administrators

--- src/components/StarCarousel.js
+++ src/components/StarCarousel.js
@@ -9,18 +9,18 @@ export default function StarCarousel() {
 
     //get starUrl from promise
     const [starUrl, setStarUrl] = useState([])
-    const [starName, setStarName] = useState([])
     const getStarUrl = async () => {
-        const starUrl = await w3.GetStars1();
-        const urls = starUrl[0].map((url) => {
-            return url[3]  //get the url from the array
+        const [starUrl] = await w3.GetStars1();  //get array[0] of the array result which back from GetStars1 function
+        const starInfo = starUrl.map((item) => {  //change the array to object with map function
+            const [,name,introduction,url,vote] = item
+            return {url,name,introduction,vote:vote.toString()}  
         })
-        const names = starUrl[0].map((name) => {
-            return name[1]  //get the name from the array 
-        })
-        setStarUrl(urls)
-        setStarName(names)
-        console.log('starUrl', starUrl, urls)
+        .sort((a,b)=>b.vote-a.vote)
+        .slice(0,3)
+
+        setStarUrl(starInfo)
+        console.log('starUrl', starUrl, starInfo)
+        console.log('starInfo', starInfo)
     }
 
     useEffect(() => {
@@ -30,17 +30,17 @@ export default function StarCarousel() {
     return (
         <Carousel>
             {
-                starUrl.map((url) => {
+                starUrl.map((item) => {
                     return (
-                        <Carousel.Item style={{ height: '700px' }} key={url}>
+                        <Carousel.Item style={{ height: '700px' }} key={item.url}>
                             <img
                                 className="d-block w-100"
-                                src={url}
+                                src={item.url}
                                 alt="First slide"
                             />
                             <Carousel.Caption>
-                                <h3 style={{ color: "#ffffff" }}>First slide</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                <h3 style={{ color: "#ffffff" }}>{item.name}</h3>
+                                <p>{item.introduction}</p>
                             </Carousel.Caption>
                         </Carousel.Item>
                     )
--- src/pages/Myaccount.js
+++ src/pages/Myaccount.js
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card'
 import Table from 'react-bootstrap/Table'
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
+import ListGroup from 'react-bootstrap/ListGroup'
 import { Link } from "react-router-dom";
 import { Button, Container } from "react-bootstrap";
 import RefundConfirm from './RefundConfirm'
@@ -20,7 +21,6 @@ export default function Myaccount() {
 
     //get balance info
     const [balance, setBalance] = useState("-");
-    // const [ethBalance, setEthBalance] = useState("-"); 
     const [currentEther, setCurrentEther] = useState("0");
     async function getBalance() {
         const balance = await w3.GetBalanceInfo()
@@ -56,20 +56,6 @@ export default function Myaccount() {
         getTransac()
     }, [])
 
-
-    //get balance from promise
-    // const [balance, setBalance] = useState(0)
-    // const getBalance = async () => {
-    //     const balance = await w3.GetBalanceInfo()
-    //     setBalance(balance)
-    //     console.log(balance)
-    // }
-    // const balanceEther = balance.toString().split(',')
-    // console.log('balanceEther', balanceEther[2])
-    // useEffect(() => {
-    //     getBalance()
-    // }, [])
-
     //get admin from promise
     const [isShow, setIsShow] = useState(false)
     const [admin, setAdmin] = useState(null)
@@ -87,45 +73,22 @@ export default function Myaccount() {
         console.log(admin)
     }, [])
 
-
     return (
         <>{isShow &&
             <>{admin ?
                 <div>
-                    <Container className='form-border'>
-                        <Card border="primary" style={{ width: '15rem' }}>
-                            <Card.Header style={{ height: '2rem' }}>Account Balance</Card.Header>
-                            <Card.Body>
-                                <Card.Title>{balanceInfo[1]} TokenPrice</Card.Title>
-                                <Button variant="primary" onClick={handleShow}>
-                                    Refund
-                                </Button>
-                                <Modal show={show} onHide={handleClose}>
-                                    <Modal.Header closeButton>
-                                        <Modal.Title>Refund</Modal.Title>
-                                    </Modal.Header>
-                                    <Modal.Body>
-                                        <Form>
-                                            <Form.Group className="mb-3 input-group input-group-outline" controlId="formBasicName">
-                                                <Form.Control type="number" placeholder="Input Account" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                                                <Form.Text className="text-muted">
-                                                    Available Refunds(Votes):{balanceInfo[0]}
-                                                </Form.Text>
-                                            </Form.Group>
-                                        </Form>
-                                    </Modal.Body>
-                                    <Modal.Footer>
-                                        <Button variant="secondary" onClick={handleClose}>
-                                            Close
-                                        </Button>
-                                        <Button variant="primary" onClick={handleShow}>
-                                            <Link to="/refundconfirm">Confirm</Link>
-                                        </Button>
-                                    </Modal.Footer>
-                                </Modal>
-                            </Card.Body>
-                        </Card>
-                    </Container>
+                    <Card border="primary" style={{ width: '60rem' }}>
+                        {/* <Card.Header style={{ height: '2rem' }}></Card.Header> */}
+                        <Card.Body>
+                            <Card.Title>Account Balance</Card.Title>
+                            <ListGroup>
+                                <ListGroup.Item variant="danger">TotalToken:{balanceInfo[0]}</ListGroup.Item>
+                                <ListGroup.Item variant="success">TokenPrice:{balanceInfo[1]}</ListGroup.Item>
+                                <ListGroup.Item variant="warning">Balance:{balanceInfo[2]}</ListGroup.Item>
+                                <ListGroup.Item variant="info">Contract_ether:{balanceInfo[3]}</ListGroup.Item>
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
                 </div> :
                 <div>
                     <Card border="primary" style={{ width: '15rem' }}>
@@ -144,6 +107,7 @@ export default function Myaccount() {
                                         <Form.Group className="mb-3 input-group input-group-outline" controlId="formBasicName">
                                             <Form.Control type="number" placeholder="Input Account" aria-label="Recipient's username" aria-describedby="basic-addon2"
                                                 value={currentEther} onInput={e => setCurrentEther(e.target.value)} />
+                                            <br />
                                             <Form.Text className="text-muted">
                                                 Available Refunds(Votes):{balanceInfo[0]}
                                             </Form.Text>
@@ -185,10 +149,6 @@ export default function Myaccount() {
                         </tbody>
                     </Table>
                 </div>
-
-
-
-
             }</>
         }</>
     )
